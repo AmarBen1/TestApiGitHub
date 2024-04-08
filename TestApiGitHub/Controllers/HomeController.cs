01fa630b6505bb2e88655aa29f2a014bd5a56346
GT500 . <fap119@hotmail.com>
2024-04-08 14:19:13 +0200

    index2

--- TestApiGitHub/Controllers/HomeController.cs
+++ TestApiGitHub/Controllers/HomeController.cs
@@ -8,5 +8,9 @@ namespace TestApiGitHub.Controllers
         {
             return View();
         }
+        public IActionResult Index2()
+        {
+            return View();
+        }
     }
 }
